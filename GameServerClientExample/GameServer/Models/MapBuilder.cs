58434b37e61827524eeacd0a437cb5e471f7c8ae
MissedSte4k <36638029+MissedSte4k@users.noreply.github.com>
2019-10-14 19:57:52 +0300

    Update MapBuilder.cs
    Wall in Mab builder changed to MapObject

--- GameServerClientExample/GameServer/Models/MapBuilder.cs
+++ GameServerClientExample/GameServer/Models/MapBuilder.cs
@@ -12,7 +12,6 @@ namespace GameServer.Models
         public MapBuilder()
         {
             moList = new MapObject[15,15];
-            return;
         }
 
         public override Builder BuildDestructibleWalls()
@@ -21,7 +20,7 @@ namespace GameServer.Models
             //Pirmos eilutes sudarymas
             for (int i = 0; i < Width; i++)
             {
-                Wall wall = new Wall();
+                MapObject wall = new Wall();
                 Coordinates c = new Coordinates(i, 0);
                 wall.SetCoordinates(c);
                 moList[i, 0] = wall;
@@ -36,14 +35,14 @@ namespace GameServer.Models
                     {
                         if (x == 0)
                         {
-                            Wall wall = new Wall();
+                            MapObject wall = new Wall();
                             Coordinates c = new Coordinates(x, y);
                             wall.SetCoordinates(c);
                             moList[x, y] = wall;
                         }
                         else if (x == Width - 1)
                         {
-                            Wall wall = new Wall();
+                            MapObject wall = new Wall();
                             Coordinates c = new Coordinates(x, y);
                             wall.SetCoordinates(c);
                             moList[x, y] = wall;
@@ -58,7 +57,7 @@ namespace GameServer.Models
                     {
                         if (x % 2 == 0)
                         {
-                            Wall wall = new Wall();
+                            MapObject wall = new Wall();
                             Coordinates c = new Coordinates(x, y);
                             wall.SetCoordinates(c);
                             moList[x, y] = wall;
@@ -73,7 +72,7 @@ namespace GameServer.Models
             //Paskutines eilutes sudarymas
             for(int i = 0; i < Width; i++)
             {
-                Wall wall = new Wall();
+                MapObject wall = new Wall();
                 Coordinates c = new Coordinates(i, Width - 1);
                 wall.SetCoordinates(c);
                 moList[i, Width - 1] = wall;
