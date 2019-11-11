61ee9815e9fd887eb69a82dd3990094092c30493
Clark-Ken <vadimas173@gmail.com>
2019-11-11 22:13:12 +0200

    .

--- GameServerClientExample/Testing/DecoratorTests.cs
+++ GameServerClientExample/Testing/DecoratorTests.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GameServer.Models;
+using GameServer.Models.AnstractFactory;
+using System.Drawing;
+using System.Threading;
+using Xunit;
 
 namespace Testing
 {
-    class DecoratorTests
+    public class DecoratorTests
     {
+        [Fact]
+        public void BombDecoratorTest()
+        {
+            
+        }
     }
 }
