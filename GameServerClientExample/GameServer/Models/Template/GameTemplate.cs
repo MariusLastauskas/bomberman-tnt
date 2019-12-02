b1b53610859de7915674203f8da30884c0a86897
Lenovis <40159680+Lenovis@users.noreply.github.com>
2019-12-02 18:15:31 +0200

    Template metodas

---
+++ GameServerClientExample/GameServer/Models/Template/GameTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace GameServer.Models.Template
+{
+    public class GameTemplate
+    {
+        public virtual void setPlayers(Player p1, Player p2) { }
+
+        public virtual void pingPlayer(ref DateTime ping) { }
+
+        public virtual bool checkPing(DateTime ping) { return false; }
+
+        public virtual bool CheckGameState() { return false; }
+
+        public virtual bool DisconnectPlayer(string authToken) { return false; }
+
+        public virtual bool DisconnectPlayer() { return false; }
+
+        public virtual void StartGame() { }
+
+        public virtual bool MovePlayer(string authToken, string direction) { return false; }
+
+        public virtual bool PlantBomb(string authToken) { return false; }
+    }
+}
