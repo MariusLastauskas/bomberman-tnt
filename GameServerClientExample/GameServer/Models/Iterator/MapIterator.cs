c4f90bc64ea08fdc154b4625ab7303c7b1d491a3
MariusLastauskas <marius20122@gmail.com>
2019-12-06 19:07:29 +0200

    MapIterator added

---
+++ GameServerClientExample/GameServer/Models/Iterator/MapIterator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameServer.Models.Iterator
+{
+    public class MapIterator : myIterator<MapObject>
+    {
+        private Map map { get; set; }
+        private Coordinates indexCoordinates;
+        private int index;
+
+        public MapIterator(Map map)
+        {
+            this.map = map;
+            indexCoordinates = new Coordinates(0, 0);
+            index = 0;
+        }
+
+        public MapObject first()
+        {
+            indexCoordinates.PosX = 0;
+            indexCoordinates.PosY = 0;
+            index = 0;
+            return map.getObjectIn(indexCoordinates)[index];
+        }
+
+        public MapObject next()
+        {
+            if (map.getObjectIn(indexCoordinates).Count > index + 1)
+            {
+                index++;
+            } else
+            {
+                index = 0;
+                if (indexCoordinates.PosX < 15)
+                {
+                    indexCoordinates.PosX++;
+                } else
+                {
+                    indexCoordinates.PosX = 0;
+                    indexCoordinates.PosY++;
+                }
+            }
+
+            return map.getObjectIn(indexCoordinates)[index];
+        }
+
+        public bool hasNext()
+        {
+            if (indexCoordinates.PosX < 15 || indexCoordinates.PosY < 15)
+            {
+                return true;
+            }
+            return map.getObjectIn(indexCoordinates).Count > index + 1;
+        }
+
+        public void remove()
+        {
+            map.removeObject(map.getObjectIn(indexCoordinates)[index]);
+        }
+    }
+}
