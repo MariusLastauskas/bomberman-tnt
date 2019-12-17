fb6fa721157f99f93e03dfb20ecedb413b8f5eea
MariusLastauskas <marius20122@gmail.com>
2019-12-17 07:33:48 +0200

    stable version

--- GameServerClientExample/GameServer/Models/Prototype/MapWithUndestructibleWalls.cs
+++ GameServerClientExample/GameServer/Models/Prototype/MapWithUndestructibleWalls.cs
@@ -10,7 +10,7 @@ namespace GameServer.Models
         public MapWithUndestructibleWalls()
         {
             MapBuilder newMap = new MapBuilder();
-            newMap.BuildIndestructibleWalls().AddPlayers();
+            newMap.BuildIndestructibleWalls();
             moList = newMap.getMoList();
         }
 
